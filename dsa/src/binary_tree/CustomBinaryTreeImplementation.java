d8d925d6c371d48e00ec411af3ea8f00e2cd55e8
yatin166 <yatin16694@gmail.com>
2024-03-27 19:15:56 +0100

    inorder, preorder and post order traversal

--- dsa/src/binary_tree/CustomBinaryTreeImplementation.java
+++ dsa/src/binary_tree/CustomBinaryTreeImplementation.java
@@ -79,10 +79,58 @@ public class CustomBinaryTreeImplementation {
         prettyDisplay(node.left, level + 1);
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.val + ", ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.val + ", ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.val + ", ");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomBinaryTreeImplementation binaryTree = new CustomBinaryTreeImplementation();
         binaryTree.populate(scanner);
         binaryTree.display();
+        System.out.println("Pre-order traversal");
+        binaryTree.preOrder();
+        System.out.println("In-order traversal");
+        binaryTree.inOrder();
+        System.out.println("Post-order traversal");
+        binaryTree.postOrder();
     }
 }
