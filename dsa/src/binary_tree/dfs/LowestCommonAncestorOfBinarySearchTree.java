a35cd1e07c8842328507ce7189e6d595b33a6a3b
yatin166 <yatin16694@gmail.com>
2024-04-09 20:19:40 +0200

    lowest common ancestor of BST DFS

--- dsa/src/binary_tree/dfs/LowestCommonAncestorOfBinarySearchTree.java
+++ dsa/src/binary_tree/dfs/LowestCommonAncestorOfBinarySearchTree.java
@@ -9,9 +9,29 @@ public class LowestCommonAncestorOfBinarySearchTree {
         return root;
     }
 
+    private static TreeNode lowestCommonAncestorUsingDFS(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
 
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestorUsingDFS(root.left, p, q);
+        TreeNode right = lowestCommonAncestorUsingDFS(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left == null ? right : left;
+    }
 
     public static void main(String[] args) {
-
+        TreeNode q = new TreeNode(4, new TreeNode(3), new TreeNode(5));
+        TreeNode p = new TreeNode(2, new TreeNode(0), q);
+        TreeNode tree = new TreeNode(6, p, new TreeNode(8, new TreeNode(7), new TreeNode(9)));
+        System.out.println(lowestCommonAncestor(tree, p, q).val);
     }
 }
