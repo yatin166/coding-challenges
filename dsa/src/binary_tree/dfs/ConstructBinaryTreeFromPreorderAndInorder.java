644bbdb625d0d19f3a6d84f6513031718b3d293d
yatin166 <yatin16694@gmail.com>
2024-04-13 13:07:32 +0200

    construct binary tree from pre order and inorder using map and DFS

--- dsa/src/binary_tree/dfs/ConstructBinaryTreeFromPreorderAndInorder.java
+++ dsa/src/binary_tree/dfs/ConstructBinaryTreeFromPreorderAndInorder.java
@@ -1,6 +1,8 @@
 package binary_tree.dfs;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConstructBinaryTreeFromPreorderAndInorder {
 
@@ -22,10 +24,36 @@ public class ConstructBinaryTreeFromPreorderAndInorder {
         return node;
     }
 
+    // Using HashMap
+    static int index = 0;
+    private static TreeNode buildTreeUsingHashMapDFS(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return helper(preorder, inorder, 0, preorder.length - 1, map);
+    }
+
+    private static TreeNode helper(int[] preorder, int[] inorder, int left, int right, Map<Integer, Integer> map) {
+        if (left > right) {
+            return null;
+        }
+        int currentNodeVal = preorder[index];
+        index++;
+        TreeNode node = new TreeNode(currentNodeVal);
+        if (left == right) {
+            return node;
+        }
+        int inorderIndex = map.get(currentNodeVal);
+        node.left = helper(preorder, inorder, left, inorderIndex - 1, map);
+        node.right = helper(preorder, inorder, inorderIndex + 1, right, map);
+        return node;
+    }
+
     public static void main(String[] args) {
         int[] preorder = {3,9,20,15,7};
         int[] inorder = {9,3,15,20,7};
-        TreeNode tree = buildTree(preorder, inorder);
-        tree.prettyDisplay();
+        TreeNode tree = buildTreeUsingHashMapDFS(preorder, inorder);
+        tree.printTree();
     }
 }
