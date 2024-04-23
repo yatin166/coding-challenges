b76a3d5f40a29fa44157c6e50852b4bd8dc959f6
yatin166 <yatin16694@gmail.com>
2024-04-23 17:33:19 +0200

    recover BST with clean DFS

--- dsa/src/binary_tree/dfs/RecoverBinarySearchTree.java
+++ dsa/src/binary_tree/dfs/RecoverBinarySearchTree.java
@@ -5,6 +5,7 @@ public class RecoverBinarySearchTree {
     static TreeNode firstNode = null;
     static TreeNode secondNode = null;
     static TreeNode previousNode = new TreeNode(Integer.MIN_VALUE);
+    static TreeNode previous = null;
     private static void findMistakenNodes(TreeNode root) {
         if (root == null) {
             return;
@@ -37,9 +38,47 @@ public class RecoverBinarySearchTree {
         return root;
     }
 
+    private static TreeNode helper(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        helper(node.left);
+
+        if (previous != null && previous.val > node.val) {
+            if (firstNode == null) {
+                firstNode = previous;
+            }
+            secondNode = node;
+        }
+
+        previous = node;
+
+        helper(node.right);
+
+        return node;
+    }
+    private static TreeNode recoverTreeUsingDFSAndClean(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        helper(root);
+
+        int temp = firstNode.val;
+        firstNode.val = secondNode.val;
+        secondNode.val = temp;
+
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode tree = new TreeNode(3, new TreeNode(1), new TreeNode(4, new TreeNode(2), null));
-        TreeNode recoveredTree = recoverTreeUsingDFS(tree);
+        System.out.println("BEFORE:");
+        tree.printTree();
+        System.out.println();
+        TreeNode recoveredTree = recoverTreeUsingDFSAndClean(tree);
+        System.out.println("AFTER:");
         recoveredTree.printTree();
     }
 }
